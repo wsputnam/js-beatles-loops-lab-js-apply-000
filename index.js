44aacc49972facd89c2586d572073e6723cb5e5e
William Putnam <williamputnamjr@gmail.com>
2017-10-10 22:31:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function theBeatlesPlay(musicians, instruments) {
   return strings;
 }
 
+function johnLennonFacts(facts) {
+  var facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  var arr = [];
+ var i = 0;
+ while (i < facts.length; i++) {
+   arr.push(facts[i] + '!!!');
+   i++;
+ }
+return arr;
+}
+
